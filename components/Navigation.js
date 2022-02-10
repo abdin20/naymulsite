697b75c3361a34796ddbe50ce96c475bc32a3423
abdin20 <naymul@hotmail.ca>
2022-02-10 02:20:03 -0500

    base template of site

---
+++ components/Navigation.js
@@ -0,0 +1,40 @@
+import Link from "next/link"
+import React from "react"
+import ThemeSwitch from "../components/ThemeSwitch"
+
+const Navigation = () => {
+    return (
+        <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
+            <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
+                <Link href="/">
+                    <a
+                        className={"font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"}
+                    >
+                        Naymul
+                    </a>
+                </Link>
+                <div className="space-x-5 container px-4 mx-auto lg:max-w-4xl flex items-center justify-end">
+                <Link href="/">
+                    <a
+                        className={"font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"}
+                    >
+                        About Me
+                    </a>
+                </Link> 
+                <Link href="/projects">
+                    <a
+                        className={"font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"}
+                    >
+                        Projects
+                    </a>
+                </Link>
+                <ThemeSwitch></ThemeSwitch>
+                </div>
+           
+           
+            </div>
+        </div>
+    )
+}
+
+export default Navigation;
---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import 'tailwindcss/tailwind.css'
+import Navigation from "../components/Navigation";
+import { ThemeProvider } from 'next-themes'
+
+import Footer from "../components/Footer";
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <ThemeProvider attribute="class" enableSystem={false}>
+    <Navigation/>
+    <Component {...pageProps} />
+    <Footer></Footer>
+    </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
